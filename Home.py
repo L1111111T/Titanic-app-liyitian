398bb6829d00737caefa7e26657e821e1f186d16
Yitian Li <liyitian@udel.edu>
2026-05-30 09:57:24 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+st.set_page_config(page_title="Home")
+
+st.title("Welcome to the Home Page!")
+st.write("This is the home page for the Titanic App.")
+
+if st.button("Go to Titanic App"):
+    st.switch_page("pages/app.py")
